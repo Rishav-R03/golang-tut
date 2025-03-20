9f2887bf6ff8109109eaea3ced1022d816052797
Rishav-R03 <rishav042023@gmail.com>
2025-03-21 00:51:15 +0530

    go if lse

--- datatypes/main.go
+++ datatypes/main.go
@@ -5,25 +5,40 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
-var Answer bool = false
-
-func isAdult(age int8) bool {
-	if age > 18 {
-		Answer = true
-	}
-	return Answer
+// isAdult determines if the given age qualifies as an adult (18+)
+func isAdult(age int) bool {
+	return age >= 18
 }
+
 func main() {
-	inputReader := bufio.NewReader(os.Stdin)
-	var userAge, _ = inputReader.ReadString('\n')
-	fmt.Println("user's age: ", userAge)
-	convertAge, _ := strconv.Atoi(userAge)
-	isUserAdult := isAdult(int8(convertAge))
-	if isUserAdult {
-		fmt.Println("You are an adult! ")
+	// Create a new buffered reader to handle user input
+	reader := bufio.NewReader(os.Stdin)
+
+	// Prompt the user for their age
+	fmt.Print("Enter your age: ")
+	ageInput, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	// Trim any leading or trailing whitespace (including newlines)
+	ageInput = strings.TrimSpace(ageInput)
+
+	// Convert the input string to an integer
+	age, err := strconv.Atoi(ageInput)
+	if err != nil {
+		fmt.Println("Invalid input. Please enter a valid number.")
+		return
+	}
+
+	// Determine if the user is an adult
+	if isAdult(age) {
+		fmt.Println("You are an adult!")
 	} else {
-		fmt.Println("You are a kid! ")
+		fmt.Println("You are a kid!")
 	}
 }
