f044ca89342be0b6e3c820de65799df03f612f64
Rishav-R03 <rishav042023@gmail.com>
2026-03-23 12:03:00 +0530

    currency aggregator

---
+++ Goroutine_Channels/currency_aggregator.go
@@ -0,0 +1,12 @@
+package main
+
+// "fmt"
+// "math/rand"
+// "time"
+
+// func fetchPrice(source string, c chan string) {
+// 	//simulate random network latency
+// 	latency := time.Duration(rand.Intn(900)+100) * time.Millisecond
+// 	time.Sleep(latency)
+// 	c <- fmt.Sprintf("Price from %s: 65000 (took %v)", source, latency)
+// }
